03b2e5a22127f156cc150d1e13ff797a6811c753
Zyko <hata.s520@gmail.com>
2026-03-06 08:26:02 +0800

    setup config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@nuxt/ui', '@nuxthub/core'],
-  css: ["~/assets/css/main.css"]
+  css: ["~/assets/css/main.css"],
+  nitro: {
+    preset: 'cloudflare-pages',
+  },
 })
