1cba11de4b1e32686aa1caa7d7739e2165da497f
Zyko <hata.s520@gmail.com>
2026-03-06 08:47:12 +0800

    fix carousel

--- app/components/BannerCarousel.vue
+++ app/components/BannerCarousel.vue
@@ -1,8 +1,14 @@
 <template>
-  <VueperSlides :autoplay="true" class="w-full h-[500px] rounded-xl">
-    <VueperSlide v-for="(slide, i) in slides" :key="i" :image="slide" />
+  <VueperSlides :autoplay="true" fixed-height="100vh" class="w-full bg-gray-100 rounded-xl">
+    <VueperSlide v-for="(slide, i) in slides" :key="i">
+      <template #content>
+        <div class="w-full h-full flex items-center justify-center">
+          <img :src="slide" class="w-full h-full object-contain" />
+        </div>
+      </template>
+    </VueperSlide>
   </VueperSlides>
-  <!-- 卡片區塊，加入上外距 -->
+  
   <div class="mt-8">
     <CardSection />
   </div>
